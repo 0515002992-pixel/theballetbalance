851d1924962989d73a78e3ad2c477c579f7d8ea4
0515002992-pixel <0515002992@cikal.co.id>
2026-04-01 09:00:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglePodcast() {
+  document.getElementById("podcast").classList.toggle("hidden");
+}
+
+function showQuote() {
+  document.getElementById("quote").classList.toggle("hidden");
+}
